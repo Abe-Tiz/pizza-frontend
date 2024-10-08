a73d484ea6f9b70c8bac67414bdd79c9ee8f698e
Abe-Tiz <106752541+Abe-Tiz@users.noreply.github.com>
2024-10-08 15:10:17 +0300

    deployed version commit

---
+++ src/sagas/menuSaga.js
@@ -0,0 +1,81 @@
+
+import { call, put, takeLatest } from "redux-saga/effects";
+import {
+  addMenuItemRequest,
+  addMenuItemSuccess,
+  addMenuItemFailure,
+  fetchMenuSuccess,
+  fetchMenuFailure,
+  fetchMenuRequest,
+  deleteMenuSuccess,
+  deleteMenuFailure,
+  deleteMenuRequest,
+} from "../redux/menuSlice";
+import axios from "axios";
+
+function* addMenuItem(action) {
+  const { name, toppings, price, photo,restaurant_id } = action.payload;
+
+  const formData = new FormData();
+  formData.append("name", name);
+
+  // Append each topping
+  toppings.forEach((topping) => {
+    formData.append("toppings", topping); 
+  });
+
+  formData.append("price", price);
+  formData.append("photo", photo);
+  formData.append("restaurant_id", restaurant_id);
+
+  try {
+    const response = yield call(
+      axios.post,
+      "http://localhost:4000/menu",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    yield put(addMenuItemSuccess(response.data));
+    console.log("saga:", response.data);
+  } catch (error) {
+    yield put(addMenuItemFailure(error.message));
+  }
+}
+
+export function* watchAddMenuItem() {
+  yield takeLatest(addMenuItemRequest.type, addMenuItem);
+}
+
+// fetch menu
+function* fetchMenuSaga() {
+  try {
+    const response = yield call(axios.get, "http://localhost:4000/menu/get-menu");
+    const data = yield response.data;
+    yield put(fetchMenuSuccess(data));
+  } catch (error) {
+    yield put(fetchMenuFailure(error.message));
+  }
+}
+
+export function* watchFetchMenu() {
+  yield takeLatest(fetchMenuRequest.type, fetchMenuSaga);
+}
+
+// fetch menu
+function* deleteMenuSaga() {
+  try {
+    const response = yield call(axios.delete, "http://localhost:4000/menu/delete/:id");
+    const data = yield response.data;
+    yield put(deleteMenuSuccess(data));
+  } catch (error) {
+    yield put(deleteMenuFailure(error.message));
+  }
+}
+
+export function* watchdeleteMenu() {
+  yield takeLatest(deleteMenuRequest.type, deleteMenuSaga);
+}
